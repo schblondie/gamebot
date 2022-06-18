262d154a67d886d70680a521986e4aaed2f00176
Felix <limette1999@gmail.com>
2022-06-18 15:43:48 +0200

    New activities

--- interactions/buttons/tools/devtools.js
+++ interactions/buttons/tools/devtools.js
@@ -14,9 +14,26 @@ module.exports = {
    */
 
   async execute(interaction) {
-    await interaction.reply({
-      content: 'This was a reply from button handler!',
+    const target = imp.prev.targetMember;
+    const prev = imp.prev;
+    const devtools_row = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId('devtools_select')
+        .setPlaceholder('Nothing selected')
+        .addOptions([
+          {
+            label: 'Change roles',
+            description: 'Dev Tools',
+            value: 'devtools_changeroles',
+          },
+        ]),
+    );
+    // Add the row to the message
+    interaction.reply({
+      content: 'Select option',
+      components: [devtools_row],
+      ephemeral: None,
+      attachments: [],
     });
-    return;
   },
 };
---
+++ interactions/select-menus/devtools/devselectmenu.js
@@ -0,0 +1,16 @@
+/**
+ * @file Select menu interaction: devselectmenu
+ * @author Felix
+ * @since 1.0.0
+ */
+module.exports = {
+  id: 'devselectmenu',
+  /**
+   * @description Executes when the select menu with ID devselectmenu is called.
+   * @author Felix
+   * @param {Object} interaction The Interaction Object of the command.
+   */
+  async execute(interaction) {
+    return;
+  },
+};
