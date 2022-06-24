034227dc457e6eeba5ec23eb99c1a800fc099ace
Felix <limette1999@gmail.com>
2022-06-24 10:39:21 +0200

    ESLint V4

--- interactions/modals/vorschlag/vorschlag_normal.js
+++ interactions/modals/vorschlag/vorschlag_normal.js
@@ -3,13 +3,9 @@
  * @author Felix
  * @since 2.0.0
  */
-const {
-    MessageEmbed,
-    MessageActionRow,
-    MessageButton
-} = require('discord.js')
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 module.exports = {
-    id: 'vorschlag_normal',
+    id: "vorschlag_normal",
 
     /**
    * @description Executes when the modal with ID "vorschlag_normal" is called.
@@ -19,14 +15,14 @@ module.exports = {
 
     async execute (interaction) {
 
-        const titel = interaction.fields.getTextInputValue('titel')
-        const beschreibung = interaction.fields.getTextInputValue('beschreibung')
-        if (titel === '' || beschreibung === '') {
+        const titel = interaction.fields.getTextInputValue("titel");
+        const beschreibung = interaction.fields.getTextInputValue("beschreibung");
+        if (titel === "" || beschreibung === "") {
 
             return interaction.reply({
-                content: '**Vorschlag unvollständig!**',
+                content: "**Vorschlag unvollständig!**",
                 ephemeral: true
-            })
+            });
 
         }
         var embed = new MessageEmbed()
@@ -37,7 +33,7 @@ module.exports = {
                 iconURL: interaction.member.displayAvatarURL({
                     dynamic: true
                 })
-            })
+            });
         interaction.channel
             .send({
                 embeds: [embed],
@@ -45,83 +41,80 @@ module.exports = {
             })
             .then(function (message) {
 
-                message.react('👍')
-                message.react('👎')
+                message.react("👍");
+                message.react("👎");
                 message.startThread({
                     name: `${titel}`,
                     autoArchiveDuration: 1440 * 7,
-                    type: 'GUILD_PUBLIC_THREAD'
-                })
+                    type: "GUILD_PUBLIC_THREAD"
+                });
 
             })
-            .catch()
+            .catch();
         async function run () {
 
             const row1 = new MessageActionRow().addComponents(
                 new MessageButton()
-                    .setLabel('Vorschlag')
-                    .setCustomId('vorschlag_normal')
-                    .setStyle('PRIMARY'),
+                    .setLabel("Vorschlag")
+                    .setCustomId("vorschlag_normal")
+                    .setStyle("PRIMARY"),
                 new MessageButton()
-                    .setLabel('Anonym')
-                    .setCustomId('vorschlag_anonym')
-                    .setStyle('SECONDARY'),
+                    .setLabel("Anonym")
+                    .setCustomId("vorschlag_anonym")
+                    .setStyle("SECONDARY"),
                 new MessageButton()
-                    .setLabel('Event')
-                    .setCustomId('vorschlag_event')
-                    .setStyle('SECONDARY')
-            )
+                    .setLabel("Event")
+                    .setCustomId("vorschlag_event")
+                    .setStyle("SECONDARY")
+            );
             const fetch = await interaction.channel.messages.fetch({
                 limit: 10
-            })
+            });
             const fetchfiltered = fetch.filter(function (list) {
 
-                return list.content === 'Drücke hier um einen Vorschlag einzureichen'
+                return list.content === "Drücke hier um einen Vorschlag einzureichen";
 
-            })
+            });
             const id = fetchfiltered.map(function (list) {
 
-                return list.id
+                return list.id;
 
-            })
+            });
             if (id.length !== 0) {
 
                 interaction.channel.messages
                     .fetch(id.toString())
                     .then((message) => {
 
-                        message.delete()
+                        message.delete();
 
                     })
-                    .catch({})
+                    .catch({});
 
             }
             interaction.channel.send({
-                content: 'Drücke hier um einen Vorschlag einzureichen',
+                content: "Drücke hier um einen Vorschlag einzureichen",
                 components: [row1]
-            })
+            });
 
         }
-        run().then().catch()
-        require('dotenv').config()
-        const api = process.env.trello_api
-        const token = process.env.trello_token
-        const Trello = require('trello-node-api')(api, token)
+        run().then().catch();
+        require("dotenv").config();
+        const api = process.env.trello_api;
+        const token = process.env.trello_token;
+        const Trello = require("trello-node-api")(api, token);
         const data = {
             name: titel,
             desc: beschreibung,
-            pos: 'top',
-            idList: '62016b6b6686ac1c549a0ed6'
-        }
-        Trello.card
-            .create(data)
-            .then()
-            .catch()
+            pos: "top",
+            idList: "62016b6b6686ac1c549a0ed6"
+        };
+        Trello.card.create(data).then().catch();
         interaction.reply({
             content:
-        '**Vorschlag eingereicht!**\nDu kannst diese Nachricht jetzt verwerfen',
+        "**Vorschlag eingereicht!**\nDu kannst diese Nachricht jetzt verwerfen",
             ephemeral: true
-        })
+        });
 
     }
-}
+};
