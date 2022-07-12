28c19562887d5296451b43494807175989c089d7
Felix <schblondie@gmail.com>
2022-07-12 13:47:15 +0200

    New modal added

--- interactions/select-menus/empfangtools/empfangselectmenu.js
+++ interactions/select-menus/empfangtools/empfangselectmenu.js
@@ -6,7 +6,7 @@
  */
 
 const {
-  MessageEmbed
+  MessageEmbed, MessageActionRow, Modal, TextInputComponent
 } = require('discord.js')
 
 const imp = require('../../buttons/tools/empfangtools')
@@ -21,7 +21,6 @@ module.exports = {
 
   async execute (interaction) {
 
-    await interaction.deferReply({ ephemeral: true})
     const target = imp.prev.targetMember
     const prev = imp.prev
     const empfangslog =
@@ -36,7 +35,7 @@ module.exports = {
       if (target.roles.cache.some((role) => role.name === 'Tourist')) {
 
         target.roles.remove(role)
-        interaction.editReply({
+        interaction.reply({
           content: '`Tourist` entfernt',
           ephemeral: true
         })
@@ -47,7 +46,7 @@ module.exports = {
       } else {
 
         target.roles.add(role)
-        interaction.editReply({
+        interaction.reply({
           content: '`Tourist` hinzugefügt',
           ephemeral: true
         })
@@ -70,7 +69,7 @@ module.exports = {
 
         target.roles.remove(role1)
         target.roles.remove(role2)
-        interaction.editReply({
+        interaction.reply({
           content: '`Tourist & Einwohner:in` entfernt',
           ephemeral: true
         })
@@ -82,7 +81,7 @@ module.exports = {
 
         target.roles.add(role1)
         target.roles.add(role2)
-        interaction.editReply({
+        interaction.reply({
           content: '`Tourist & Einwohner:in` hinzugefügt',
           ephemeral: true
         })
@@ -107,7 +106,7 @@ module.exports = {
       ) {
 
         target.roles.remove(role3)
-        interaction.editReply({
+        interaction.reply({
           content: '`Verifizierungsebene 2` entfernt',
           ephemeral: true
         })
@@ -117,62 +116,19 @@ module.exports = {
 
       } else {
 
-        target.roles.add(role3)
-        interaction.editReply({
-          content:
-            '`Verifizierungsebene 2` hinzugefügt\n\nBitte denke daran in ' +
-            ve2log.toString() +
-            ' zu dokumentieren!',
-          ephemeral: true
-        })
-        empfangslog.send({
-          content: `${interaction.user.tag} hat ${target.user} die Rolle \`Verifizierungsebene 2\` hinzugefügt`
-        })
-        const fetch = await interaction.channel.messages.fetch({
-          limit: 10
-        })
-        const fetchfiltered = fetch.filter(function (list) {
-
-          return list.author.id === target.id
-
-        })
-        var lastmsgs = fetchfiltered.map(function (list) {
-
-          return list.content
-
-        })
-        var lastmsg = lastmsgs.reverse()
-        const embed = new MessageEmbed()
-          .setAuthor({name: target.displayName + "'s letzte Nachrichten", iconURL: target.user.displayAvatarURL()})
-          .setDescription(lastmsg.join('\n\n'))
-          .setFooter({text: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL()})
-        ve2log.send({
-          content: target.toString(),
-          embeds: [embed]
-        })
-        const wartezimmer =
-          prev.member.guild.channels.cache.find(
-            (channel) => channel.name === 'wartezimmer'
-          ) || prev.member.guild.channels.cache.get('897164924994854992')
-        const ServerIcon2021 = interaction.guild.emojis.cache.find(
-          (emoji) => emoji.name === 'ServerIcon2021'
-        )
-        const QCocktail = interaction.guild.emojis.cache.find(
-          (emoji) => emoji.name === 'QCocktail'
-        )
-        target.user.send({
-          content: `**Hallo ${
-            target.user
-          } wie du vielleicht mitbekommen hast, ist der Vorstellungsprozess noch nicht ganz beendet.**
-
-					Wir laden dich hiermit herzlich zu einem Gespräch mit dem Supportteam ein. Damit wir einen gemeinsamen Termin finden, bitten wir dich um ein paar Terminvorschläge.
-					Du kannst die Terminvorschläge einfach in ${wartezimmer.toString()} senden.
-
-					Sobald wir einen passenden Termin gefunden haben werden wir uns bei dir erneut melden.
-
-					Liebe Grüße
-					Dein QueerCity Serverteam ${ServerIcon2021} ${QCocktail}`
-        })
+        const prev3 = interaction
+        const prev4 = target
+        module.exports.prev = {prev3, prev4}
+        const ve2Modal = new Modal().setCustomId('ve2Reason').setTitle('Grund');
+        const ve2Grund = new TextInputComponent()
+          .setCustomId('ve2Grund')
+          .setLabel('Grund für Verifizierungsebene 2')
+          .setStyle('PARAGRAPH');
+        const ve2GrundRow = new MessageActionRow().addComponents(ve2Grund);
+        // Add inputs to the modal
+        ve2Modal.addComponents(ve2GrundRow);
+        // Show the modal to the user
+        await interaction.showModal(ve2Modal);
 
       }
 
