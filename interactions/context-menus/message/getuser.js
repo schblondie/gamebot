aeb0971a5480e158275ea9da991fd2f03741aecb
Felix Limbach <schblondie@gmail.com>
2022-07-17 23:26:09 +0200

    Added Admin-Overpass in get-user

--- interactions/context-menus/message/getuser.js
+++ interactions/context-menus/message/getuser.js
@@ -21,11 +21,21 @@ module.exports = {
     const id = interaction.guild.id
     const msgid = JSON.stringify(await get(ref(db, id + '/anonym/messages/' + interaction.targetMessage.id))).slice(1).slice(0, -1)
     const adminRole = interaction.guild.roles.cache.get(`${JSON.stringify(await get(ref(db, id + '/anonym/config/adminRole'))).slice(2).slice(0, -1)}`)
-    let admin = adminRole.members.random()
-    while (admin !== interaction.member) {
-      admin = adminRole.members.random()
+    console.log(interaction.member.roles.cache.has(adminRole))
+    if (
+      interaction.member.roles.cache.some((role) => role.name === adminRole.name)
+    ) {
+      interaction.reply({
+        content: `Die User ID lautet:\n${msgid}`,
+        ephemeral: true
+      })
+    } else {
+      let admin = adminRole.members.random()
+      while (admin !== interaction.member) {
+        admin = adminRole.members.random()
+      }
+      interaction.reply({ content: `Dein Teil lautet:\n${msgid.slice(0, Math.floor(msgid.length / 2))}\n\nBitte warte bis dir der zweite Teil der ID mitgeteilt wird`, ephemeral: true })
+      admin.send({ content: `${interaction.user} möchte die User ID von folgender Nachricht erhalten:\nhttps://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${interaction.targetId}}\n\nDein Teil lautet:\n${msgid.slice(Math.floor(msgid.length / 2), msgid.length)}`, ephemeral: true })
     }
-    interaction.reply({ content: `Dein Teil lautet:\n${msgid.slice(0, Math.floor(msgid.length / 2))}\n\nBitte warte bis dir der zweite Teil der ID mitgeteilt wird`, ephemeral: true })
-    admin.send({ content: `${interaction.user} möchte die User ID von folgender Nachricht erhalten:\nhttps://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${interaction.targetId}}\n\nDein Teil lautet:\n${msgid.slice(Math.floor(msgid.length / 2), msgid.length)}`, ephemeral: true })
   }
 }
