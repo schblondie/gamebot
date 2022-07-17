ef1ff413ad664d4b6fd8b2495e4950a0f6004119
Felix Limbach <schblondie@gmail.com>
2022-07-17 23:04:54 +0200

    Fixed duplicate code

--- interactions/context-menus/message/deletethread.js
+++ interactions/context-menus/message/deletethread.js
@@ -17,14 +17,7 @@ module.exports = {
    */
 
   async execute (interaction) {
-    if (
-      interaction.member.roles.cache.some(
-        (role) => role.name === 'Technikchef'
-      ) ||
-      interaction.member.roles.cache.some(
-        (role) => role.name === 'Stadtsekretär:in'
-      )
-    ) {
+    async function deleteThread () {
       for (let i = 0; i < interaction.targetMessage.embeds.length; i++) {
         const thread = interaction.channel.threads.cache.find(
           (x) => x.name === interaction.targetMessage.embeds[i].title
@@ -36,21 +29,21 @@ module.exports = {
         content: 'Done',
         ephemeral: true
       })
+    }
+    if (
+      interaction.member.roles.cache.some(
+        (role) => role.name === 'Technikchef'
+      ) ||
+      interaction.member.roles.cache.some(
+        (role) => role.name === 'Stadtsekretär:in'
+      )
+    ) {
+      deleteThread()
     } else {
       const db = getDatabase()
       const id = interaction.guild.id
       if (JSON.stringify(await get(ref(db, id + '/anonym/messages/' + interaction.targetMessage.id))).slice(1).slice(0, -1) === interaction.member.id) {
-        for (let i = 0; i < interaction.targetMessage.embeds.length; i++) {
-          const thread = interaction.channel.threads.cache.find(
-            (x) => x.name === interaction.targetMessage.embeds[i].title
-          )
-          await thread.delete()
-        }
-        interaction.targetMessage.delete()
-        interaction.reply({
-          content: 'Done',
-          ephemeral: true
-        })
+        deleteThread()
       } else {
         return interaction.reply({
           content: 'Dieser Thread gehört dir nicht',
